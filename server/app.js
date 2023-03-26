6c712e3424c57168832bb9a959ed458993d5c633
Conrad Nunes <nunes.conrad93@gmail.com>
2023-03-26 22:33:30 +0530

    bootstrap theme

--- server/app.js
+++ server/app.js
@@ -7,5 +7,6 @@ var app = express();
 app.use(cors());
 app.use(express.json({limit:'50mb'}));
 app.use(express.urlencoded({limit:'50mb', extended:false}));
+app.use("/static", express.static("public"));
 
 module.exports = app;
